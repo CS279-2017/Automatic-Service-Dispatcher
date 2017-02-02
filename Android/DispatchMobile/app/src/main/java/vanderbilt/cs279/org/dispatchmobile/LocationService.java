48c8cd0807b881986d9008a2560e7e5bf682652b
Geoffery Pettet <gpettet@isis.vanderbilt.edu>
2017-02-01 23:03:16 -0600

    Adding Basic Location Service (un tested and without web interface)

---
+++ Android/DispatchMobile/app/src/main/java/vanderbilt/cs279/org/dispatchmobile/LocationService.java
@@ -0,0 +1,83 @@
+package vanderbilt.cs279.org.dispatchmobile;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.location.LocationListener;
+import android.util.Log;
+
+/**
+ * todo : untested
+ */
+public class LocationService extends Service {
+
+    private static final String TAG = LOCATION_SERVICE.getClass().getCanonicalName();
+    private LocationManager mLocationManager = null;
+    private static final int LOCATION_UPDATE_INTERVAL = 1000; // in millisec
+    private static final float LOCATION_UPDATE_DISTANCE = 10f;
+
+    private LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            // todo update web api
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    };
+
+    public LocationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initializeLocationManager();
+        try {
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    LOCATION_UPDATE_INTERVAL,
+                    LOCATION_UPDATE_DISTANCE,
+                    mLocationListener);
+        }catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "location permission error");
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if(mLocationManager != null){
+            try{
+                mLocationManager.removeUpdates(mLocationListener);
+            }catch (java.lang.SecurityException ex){
+                Log.i(TAG, "location permission error");
+            }
+        }
+    }
+
+    private void initializeLocationManager(){
+        if(mLocationManager == null){
+            mLocationManager = (LocationManager) getApplicationContext()
+                    .getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+}
