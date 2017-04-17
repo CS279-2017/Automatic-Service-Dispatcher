a1e773c36b3e85dfd5d015e9c069c1f29dfdc8ed
Geoffery Pettet <gpettet@isis.vanderbilt.edu>
2017-04-17 15:37:45 -0500

    updating pojos and mapviewfragment

--- Android/DispatchMobile/app/src/main/java/vanderbilt/cs279/org/dispatchmobile/NextTaskFrag.java
+++ Android/DispatchMobile/app/src/main/java/vanderbilt/cs279/org/dispatchmobile/NextTaskFrag.java
@@ -251,7 +251,8 @@ public class NextTaskFrag extends Fragment {
                     Bundle args = new Bundle();
                     args.putDouble(MapViewFragment.LAT_DEST_KEY, (double)task.lattitude);
                     args.putDouble(MapViewFragment.LONG_DEST_KEY, (double)task.longitude);
-                    args.putString()
+                    args.putString(MapViewFragment.WAGE_KEY, (String)task.wage);
+                    args.putString(MapViewFragment.PIN_CODE_KEY, (String)task.pinCode);
 
                     // set the fragment arguments to contain the bundle
                     mapDirTest.setArguments(args);
